2f6677f5cc47857c91db9cfc70158f732ea3e737
Francesca Tyler <francesca.t.tyler@gmail.com>
2022-01-11 21:21:59 -0600

    Initial Commit

---
+++ Cashflo/CashfloApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashfloApp.swift
+//  Cashflo
+//
+//  Created by Maxwell on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CashfloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
